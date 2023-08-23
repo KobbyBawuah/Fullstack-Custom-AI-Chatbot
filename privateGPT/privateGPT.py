62f34bc1e6fd523fed20dd3485add624706f7be5
kobbybawuah <Bawuah10@hotmail.com>
2023-08-23 16:58:19 -0400

    workflow test

--- privateGPT/privateGPT.py
+++ privateGPT/privateGPT.py
@@ -6,7 +6,6 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
-import argparse
 import time
 import sentry_sdk
 
@@ -33,21 +32,17 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 from constants import CHROMA_SETTINGS
 
 def main():
-    # division_by_zero = 1 / 0
-
-
     # Parse the command line arguments
-    args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
-    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
+    # callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
     if model_type == "LlamaCpp":
-        llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+        llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, verbose=False)
     elif model_type == "GPT4All":
-        llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+        llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, verbose=False)
     else:
         # raise exception if model_type is not supported
         raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
@@ -62,12 +57,8 @@ def main():
             continue
 
         # Get the answer from the chain
-        # start = time.time()
         res = qa(query)
-        # answer, docs = res['result'], [] if args.hide_source else res['source_documents']
         answer = res['result']
-
-        # end = time.time()
 
         # Print the result
         print("\n\n> Question:")
@@ -75,24 +66,6 @@ def main():
         # print(f"\n> Answer (took {round(end - start, 2)} s.):")
         print("Answer:")
         print(answer)
-
-        # Print the relevant sources used for the answer
-        # for document in docs:
-        #     print("\n> " + document.metadata["source"] + ":")
-        #     print(document.page_content)
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
-
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
-
-    return parser.parse_args()
-
 
 if __name__ == "__main__":
     main()
